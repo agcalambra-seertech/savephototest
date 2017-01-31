9db6595bc552fa932efe080ec6e5c40ff4789873
Allister Alambra <allisteralambra@Allisters-MacBook-Pro-2.local>
2017-01-31 08:08:36 +0800

    Saving contents for review later.

---
+++ SavePhotoTest/SavePhotoTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SavePhotoTest
+//
+//  Created by Allister Alambra on 23/11/2016.
+//  Copyright © 2016 Allister Alambra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
